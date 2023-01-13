00c855a51524a0e93d06acaf1559ae00e946ba94
Laikhram Jamjuntra <itswolfe@gmail.com>
2023-01-13 11:58:07 +0700

    test: add expectation

--- test/index.ts
+++ test/index.ts
@@ -49,15 +49,38 @@ describe("StupidCalculator", () => {
 });
 
 describe("SmarterStupidCalculator", async () => {
+  let owner: SignerWithAddress;
+  let signers: SignerWithAddress[];
+  let SmarterStupidCalculatorContract: any;
+
+  before(async () => {
+    [owner, ...signers] = await ethers.getSigners();
+
+    const SmarterStupidCalculator = await ethers.getContractFactory("SmarterStupidCalculator");
+    SmarterStupidCalculatorContract = await SmarterStupidCalculator.deploy();
+  });
+
   it("Provide totalAmount of first signer", async () => {
+    expect(await SmarterStupidCalculatorContract.totalAmountEachAddress(owner.address)).to.equal(0);
   });
 
   it("Add number 10 to owner and 20 to first singer", async () => {
+    await SmarterStupidCalculatorContract.add(10);
+    expect(await SmarterStupidCalculatorContract.totalAmountEachAddress(owner.address)).to.equal(10);
+
+    const singer1 = SmarterStupidCalculatorContract.connect(signers[0]);
+    await singer1.add(20);
+    expect(await SmarterStupidCalculatorContract.totalAmountEachAddress(signers[0].address)).to.equal(20);
   });
 
   it("Subtract number of first singer", async () => {
+    const singer1 = SmarterStupidCalculatorContract.connect(signers[0]);
+    await singer1.subtract(17);
+    expect(await SmarterStupidCalculatorContract.totalAmountEachAddress(signers[0].address)).to.equal(3);
   });
 
   it("Check amout of each address", async () => {
+    expect(await SmarterStupidCalculatorContract.totalAmountEachAddress(owner.address)).to.equal(10);
+    expect(await SmarterStupidCalculatorContract.totalAmountEachAddress(signers[0].address)).to.equal(3);
   });
 });
