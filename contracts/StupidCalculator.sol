168c56e693dd0d7f5d06246a87c27810df6d8b07
Laikhram Jamjuntra <itswolfe@gmail.com>
2023-01-13 11:53:15 +0700

    feat: add functions

--- contracts/StupidCalculator.sol
+++ contracts/StupidCalculator.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.0;
 
 contract StupidCalculator {
+  uint public totalAmount;
 
+  function add(uint number) public  {
+    totalAmount += number;
+  }
+
+  function subtract(uint number) public  {
+    totalAmount -= number;
+  }
 }
