b3db095291f89eeed7d3a9110c87d2bac4140e9f
Laikhram Jamjuntra <itswolfe@gmail.com>
2023-01-13 11:58:29 +0700

    feat: add mapping

--- contracts/SmarterStupidCalculator.sol
+++ contracts/SmarterStupidCalculator.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.0;
 
 contract SmarterStupidCalculator {
+  mapping(address => uint) public totalAmountEachAddress;
 
+  function add(uint number) public  {
+    totalAmountEachAddress[msg.sender] += number;
+  }
+
+  function subtract(uint number) public  {
+    totalAmountEachAddress[msg.sender] -= number;
+  }
 }
